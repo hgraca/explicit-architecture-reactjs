18904000ad4fbe63cf11ab2ddbd2ad37181c61c5
Herberto Graca <herberto.graca@gmail.com>
2020-03-12 22:10:50 +0100

    Restructure framework config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './Infrastructure/Framework/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
