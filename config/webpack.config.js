7f2be676f89a3c442c336aaf8308295cec052463
Herberto Graca <herberto.graca@gmail.com>
2020-03-13 16:06:48 +0100

    Make webpack compile .graphql files
    So that instead of doing this in our components
    
    const SOME_QUERY = gql`
      query SomeQuery($someVar: String!) {
        ...
      }
    `;
    
    we can simply import the file with the query
    
    import SOME_QUERY from './SomeQuery.graphql';

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -483,6 +483,11 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            {
+              test: /\.(graphql|gql)$/,
+              loader: 'graphql-tag/loader',
+              exclude: /node_modules/,
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
