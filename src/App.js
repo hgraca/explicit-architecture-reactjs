19b739ed5d79183727aa6794258ddb98a405cb76
Herberto Graca <herberto.graca@gmail.com>
2020-03-17 19:09:19 +0100

    Add browser router to the project
    So that we can have local routes in the SPA.
    The router added was "react-router-dom".

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import GitHubGqlClient from "./Infrastructure/GraphQLClient/GitHubGqlClient.js";
+import { Route, Switch } from 'react-router-dom'
 
 const App = () => (
   <ApolloProvider client={ GitHubGqlClient }>
-    <div>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ SomeComponent }/>
+    </Switch>
   </ApolloProvider>
 );
 
